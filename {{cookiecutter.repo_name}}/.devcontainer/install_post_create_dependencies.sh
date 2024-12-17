1c6652c059e7f8185e59d1532ef4e0a525e962f4
Badreddine <badreddine.ben.nouma@gmail.com>
2024-12-17 10:05:10 -0500

    update python version

---
+++ {{cookiecutter.repo_name}}/.devcontainer/install_post_create_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package list
+sudo apt-get update
+
+# Install Python Poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Install project dependencies using Poetry
+pip install pre-commit && poetry install --sync --with azure,pipelines,training,dev --no-root
